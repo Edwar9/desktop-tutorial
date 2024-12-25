581b8bd36b2bef8eea88cae040b76ebff87a87ac
Eduard Ryzhov <ewar26353@gmail.com>
2024-12-25 14:30:54 +0300

    Добавил картинку в компонент о нас и добавил линию

--- hw_diplom/diplom/src/componets/About/about.tsx
+++ hw_diplom/diplom/src/componets/About/about.tsx
@@ -4,17 +4,23 @@ const About = () => {
   return (
     <div className={style.about}>
       <div className={style.content}>
-        <h1 className={style.title}>Пара слов о нас</h1>
-        <p className={style.text}>
-          Спорт держит нас в форме. Учит дисциплине. Объединяет нас. Через спорт
-          мы можем менять жизни. В том числе с помощью воодушевляющих историй
-          спортсменов. Чтобы помочь тебе подняться и двигаться вперед.{" "}
-        </p>
-        <p className={style.logo}></p>
+        <div className={style.left}>
+          <h1 className={style.title}>Пара слов о нас</h1>
+          <p className={style.text}>
+            Спорт держит нас в форме. Учит дисциплине. Объединяет нас. Через
+            спорт мы можем менять жизни. В том числе с помощью воодушевляющих
+            историй спортсменов. Чтобы помочь тебе подняться и двигаться вперед.{" "}
+          </p>
+          <div className={style.lineLogo}>
+            <span className={style.line}></span>
+            <span className={style.logo}>SneakMax</span>
+          </div>
+        </div>
+        <div className={style.right}>
+        </div>
       </div>
-      <div className={style.img}></div>
     </div>
   );
 };
 
-export default About
+export default About;
